dd31d73f222ba7876d9c203524b391a49ca360d4
jiin972 <jiin972@gmail.com>
2026-03-05 07:31:09 +0900

    feat: DB조회 및 레더링을 위한 비동기 함수 추가

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,14 +1,31 @@
+import ListProduct from "@/components/list-product";
+import db from "@/lib/db";
+
 //비동기 함수(getProducts)생성
-const getProducts = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-};
+//db.product를 findMany로 데이터 추출
+async function getProducts() {
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+  });
+  //user products를 return
+  return products;
+}
 
 //함수의 비동기화
+//prop은 {...products}의 전개연산자로 db data  전달
 export default async function Products() {
   const products = await getProducts();
   return (
-    <div>
-      <h1 className="text-white text-4xl">Products!</h1>
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
     </div>
   );
 }
