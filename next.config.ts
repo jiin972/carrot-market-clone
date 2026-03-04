880a04232dfffba5edc75ce57213e5245da1499f
jiin972 <jiin972@gmail.com>
2026-03-05 06:46:20 +0900

    fixed: trun off images optimize option

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    unoptimized: true, //로컬/외부 이미지 최적화 서버를 우회해 서빙, 리눅스환경문제,배포시 활성화
+  },
 };
 
 export default nextConfig;
