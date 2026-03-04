79534bea09a84b1541fbafd5726d2805eacf9f80
jiin972 <jiin972@gmail.com>
2026-03-04 11:59:47 +0900

    update routes

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,4 +1,11 @@
-export default function Products() {
+//비동기 함수(getProducts)생성
+const getProducts = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+};
+
+//함수의 비동기화
+export default async function Products() {
+  const products = await getProducts();
   return (
     <div>
       <h1 className="text-white text-4xl">Products!</h1>
