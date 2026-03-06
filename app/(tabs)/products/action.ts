9c960ff8f8d8c79599c0f2ce22aace16e4dc0c6d
jiin972 <jiin972@gmail.com>
2026-03-06 14:57:40 +0900

    feat:상품 목록 페이지 전용 서버 액션

--- app/(tabs)/products/action.ts
+++ app/(tabs)/products/action.ts
@@ -2,6 +2,12 @@
 
 import db from "@/lib/db";
 
+/**
+ * 상품 목록 페이지 전용 서버 액션
+ * 역할: 사용자가 '더 보기' 버튼을 누르거나 스크롤을 내릴 때,
+ * 기존에 본 상품들을 제외하고 다음 상품 데이터를 DB에서 가져옴.
+ **/
+
 export async function getMoreProducts(page: number) {
   const products = await db.product.findMany({
     select: {
