f2290d4cd8469cf6ded5e13cc728b8a5713f8849
jiin972 <jiin972@gmail.com>
2026-03-06 14:44:36 +0900

    update: 정렬 및 상품목록 렌더링방식 변경,map->컴포넌트

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,9 +1,10 @@
-import ListProduct from "@/components/list-product";
+import ProductList from "@/components/product-list";
 import db from "@/lib/db";
+import { Prisma } from "@prisma/client";
 
-//비동기 함수(getProducts)생성
+//비동기 함수(getProducts)생성 - InitialProduct용
 //db.product를 findMany로 데이터 추출
-async function getProducts() {
+async function getInitialProducts() {
   const products = await db.product.findMany({
     select: {
       title: true,
@@ -12,20 +13,27 @@ async function getProducts() {
       photo: true,
       id: true,
     },
+    take: 1, // 가져올 수량
+    orderBy: {
+      created_at: "desc", //데이터 정렬조건 생성, "asc or desc"
+    },
   });
   //user products를 return
   return products;
 }
 
+//initialProducts의 타입을 prisma를 이용해 export
+export type InitialProducts = Prisma.PromiseReturnType<
+  typeof getInitialProducts
+>;
+
 //함수의 비동기화
 //prop은 {...products}의 전개연산자로 db data  전달
 export default async function Products() {
-  const products = await getProducts();
+  const initialProducts = await getInitialProducts(); //최초 페이지만 노출되도록 수정(3/6)
   return (
-    <div className="p-5 flex flex-col gap-5">
-      {products.map((product) => (
-        <ListProduct key={product.id} {...product} />
-      ))}
+    <div>
+      <ProductList initialProducts={initialProducts} />
     </div>
   );
 }
