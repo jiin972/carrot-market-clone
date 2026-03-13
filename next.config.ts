a2bcf6a4a7f0ea2ad71b506c6bba3be3be81c993
jiin972 <jiin972@gmail.com>
2026-03-13 10:28:48 +0900

    update: "use cache"사용을 위해 "cache components" 사용을 활성화 함

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  cacheComponents: true, //"use cache" 사용을 위해 설정을 켬
   reactCompiler: true,
   images: {
     unoptimized: true, //로컬/외부 이미지 최적화 서버를 우회해 서빙, 리눅스환경문제,배포시 활성화
