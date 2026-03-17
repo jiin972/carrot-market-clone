bc8e5162889bdf1e82fa45d06d8ae56cac450039
jiin972 <jiin972@gmail.com>
2026-03-17 14:06:49 +0900

    feat: cacheComponents 적용 및 Suspense 리팩터링 - profile 페이지 Suspense적용 -modal페이지 use cache추가

--- app/(tabs)/profile/page.tsx
+++ app/(tabs)/profile/page.tsx
@@ -1,6 +1,15 @@
 import db from "@/lib/db";
 import getSession from "@/lib/session";
 import { notFound, redirect } from "next/navigation";
+import { Suspense } from "react";
+
+export default async function Profile() {
+  return (
+    <Suspense fallback={null}>
+      <ProfileContent />
+    </Suspense>
+  );
+}
 
 async function getUser() {
   const session = await getSession();
@@ -17,7 +26,7 @@ async function getUser() {
   notFound();
 }
 
-export default async function Profile() {
+async function ProfileContent() {
   const user = await getUser();
   const logOut = async () => {
     "use server";
@@ -34,3 +43,38 @@ export default async function Profile() {
     </div>
   );
 }
+
+// async function getUser() {
+//   const session = await getSession();
+//   if (session.id) {
+//     const user = await db.user.findUnique({
+//       where: {
+//         id: session.id,
+//       },
+//     });
+//     if (user) {
+//       return user;
+//     }
+//   }
+//   notFound();
+// }
+
+// export default async function Profile() {
+//   const user = await getUser();
+//   const logOut = async () => {
+//     "use server";
+//     const session = await getSession();
+//     await session.destroy();
+//     redirect("/");
+//   };
+//   return (
+//     <div>
+//       <Suspense fallback={null}>
+//         {" "}
+//         <h1>Welcom {user?.username}</h1>
+//         <form action={logOut}>
+//           <button>Log out</button>
+//         </form>
+//       </Suspense>
+//     </div>
+//   );
