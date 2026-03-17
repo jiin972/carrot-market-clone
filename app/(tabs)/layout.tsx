3369cae604305488a1f27df2da9d99e921638942
jiin972 <jiin972@gmail.com>
2026-03-17 14:05:07 +0900

    update: usePathname 훅 사용에 따른 빌드 중단 물제를 Suspense 경계로 해결

--- app/(tabs)/layout.tsx
+++ app/(tabs)/layout.tsx
@@ -1,13 +1,16 @@
 import TabBar from "@/components/tab-bar";
+import { Suspense } from "react";
 
 export default function TabLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       {/*각 페이지의 본문이 그려질 자리 */}
       {children}
-
-      {/*본문 아래 항상 붙어다닐 하단 바의 위치 */}
-      <TabBar />
+      <Suspense fallback={null}>
+        {" "}
+        {/*본문 아래 항상 붙어다닐 하단 바의 위치 */}
+        <TabBar />
+      </Suspense>
     </div>
   );
 }
