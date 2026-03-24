94f2c34c3d0ea66041608f2b715992a68c245375
jiin972 <jiin972@gmail.com>
2026-03-24 15:04:20 +0900

    feat: db data호출을 통한 life페이지 구현
    - db호출 시 "use cache"적용
    -db의 order는 내림차순으로 최신 게시물 상단 노출
    - metadata생성
    - tailwindCSS적용

--- app/(tabs)/life/page.tsx
+++ app/(tabs)/life/page.tsx
@@ -1,7 +1,74 @@
-export default function Life() {
+import TimeAgo from "@/components/time-ago";
+import db from "@/lib/db";
+import {
+  ChatBubbleBottomCenterIcon,
+  HandThumbUpIcon,
+} from "@heroicons/react/24/solid";
+import { cacheLife } from "next/cache";
+import Link from "next/link";
+
+async function getPosts() {
+  "use cache";
+  cacheLife("minutes");
+  // await new Promise((resolve) => setTimeout(resolve, 5000)); //스켈레톤 테스트 코드 작성
+  const posts = await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      views: true,
+      created_at: true,
+      _count: {
+        select: {
+          comments: true,
+          likes: true,
+        },
+      },
+    },
+    orderBy: {
+      created_at: "desc", //최신순으로 게시물 정렬
+    },
+  });
+  return posts;
+}
+
+export const metadata = {
+  title: "동네생활",
+};
+
+export default async function Life() {
+  const posts = await getPosts();
   return (
-    <div>
-      <h1 className="text-white text-4xl">Life!</h1>
+    <div className="p-5 flex flex-col">
+      {posts.map((post) => (
+        <Link
+          key={post.id}
+          href={`/posts/${post.id}`}
+          className={`pb-5 mb-5 border-b border-neutral-500 text-neutral-400 flex flex-col
+            last:pb-0 last:border-b-0
+            `}
+        >
+          <h2 className="text-white text-lg font-semibold">{post.title}</h2>
+          <p className="mb-2">{post.description}</p>
+          <div className="flex items-center justify-between">
+            <div className="flex gap-4 items-center ">
+              <TimeAgo time={post.created_at.toString()}></TimeAgo>
+              <span>🢝</span>
+              <span>조회 {post.views}</span>
+            </div>
+            <div className="flex gap-4 items-center *:flex *:gap-4 *:items-center">
+              <span>
+                <HandThumbUpIcon className="size-4" />
+                {post._count.likes}
+              </span>
+              <span>
+                <ChatBubbleBottomCenterIcon className="size-4" />
+                {post._count.comments}
+              </span>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
