0ea87bc0a0a2ccbb3da2cbf4ada0c1cf390938d2
jiin972 <jiin972@gmail.com>
2026-03-26 06:16:32 +0900

    feat: TimeAgo 클라이언트 컴포넌트 생성
    Date객체는 런타임 요소라 cache안에서 사용불가
    
    "use client" 컴포넌트로 분리해 클라이언트에서 시간 계산
    
    prop: post.created_at.toString()으로 전달

--- components/time-ago.tsx
+++ components/time-ago.tsx
@@ -2,6 +2,8 @@
 
 import { formatToTimeAgo } from "@/lib/util";
 
+// TimeAgo: Date객체는 런타임 요소라 "use cache"안에서 사용불가
+// -> "use client" 컴포넌트로 분리해서 클라이언트에서 처리
 export default function TimeAgo({ time }: { time: string }) {
   return (
     <div>
