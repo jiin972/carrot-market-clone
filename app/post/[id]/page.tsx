eef976258bfad7a92a0539ecef98a076bdf2d0f2
jiin972 <jiin972@gmail.com>
2026-03-26 13:42:26 +0900

    fix:기존의 서버액션을 별도의 action파일로 분리(리팩터링) - useOptimistic사용을 위해 likePost/disLikePost 를 action파일로 생성 - 관련 JSX모두 분리함

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,8 +1,8 @@
+import LikeButton from "@/components/like-button";
 import TimeAgo from "@/components/time-ago";
 import db from "@/lib/db";
 import getSession from "@/lib/session";
-import { HandThumbUpIcon as OutlineHandThumbUpIcon } from "@heroicons/react/24/outline";
-import { EyeIcon, HandThumbUpIcon } from "@heroicons/react/24/solid";
+import { EyeIcon } from "@heroicons/react/24/solid";
 import { cacheLife, cacheTag, revalidateTag } from "next/cache";
 import Image from "next/image";
 import { notFound } from "next/navigation";
@@ -77,35 +77,6 @@ async function PostContents({ params }: { params: Promise<{ id: string }> }) {
   const post = await getPost(postId);
   if (!post) return notFound(); //post가 없을 경우(null), 404처리
 
-  const likePost = async () => {
-    "use server"; //form action으로 클라이언트에서 호출되지만, 서버에서 실행되어야 하므로 use server 선언
-    try {
-      await db.like.create({
-        data: {
-          //composite ID 호출
-          postId: postId,
-          userId: session.id!, //미들웨어를 통해 session.id보자(로그인 검증완료)
-        },
-      });
-      revalidateTag(`like-status-${postId}`, { expire: 0 }); //URL에서 온 ID를 Tag의 Id로 사용
-    } catch (errors) {}
-  };
-  const disLikePost = async () => {
-    "use server";
-    try {
-      await db.like.delete({
-        where: {
-          id: {
-            //composite ID 호출
-            postId: postId,
-            userId: session.id!, //미들웨어를 통해 session.id보장(로그인 검증완료)
-          },
-        },
-      });
-      revalidateTag(`like-status-${postId}`, { expire: 0 });
-    } catch (errors) {}
-  };
-
   //리팩터링, session.id를 userId로 전달, getLikeStatus에서 likeCount/isLiked 반환
   const { likeCount, isLieked } = await getLikeStatus(postId, session.id!);
 
@@ -134,25 +105,11 @@ async function PostContents({ params }: { params: Promise<{ id: string }> }) {
             <EyeIcon className="size-5" />
             <span>조회 {post.views}</span>
           </div>
-          <form action={isLieked ? disLikePost : likePost}>
-            <button
-              className={`flex items-center gap-2 text-neutral-500 text-sm border border-neutral-400 rounded-full p-2  transition-colors
-                ${isLieked ? "bg-orange-400 text-white font-semibold border-orange-400" : "hover:bg-neutral-800"}
-                `}
-            >
-              {isLieked ? (
-                <>
-                  <HandThumbUpIcon className="size-5" />
-                  <span>{likeCount}</span>
-                </>
-              ) : (
-                <>
-                  <OutlineHandThumbUpIcon className="size-5" />
-                  <span>공감하기({likeCount})</span>
-                </>
-              )}
-            </button>
-          </form>
+          <LikeButton
+            isLieked={isLieked}
+            likeCount={likeCount}
+            postId={postId}
+          />
         </div>
       </div>
     </Suspense>
