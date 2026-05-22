90c0c4b0edd1889a877487f16cf63bf4d5e43e45
jiin972 <jiin972@gmail.com>
2026-05-22 14:11:32 +0900

    feat: review작성 및 db등록 기능 구현 - client컴포넌트: review-form.tsx , productId,sellerId,sellerName,serverAction을 props로 받음 - useState를 이용해 rating 기능 구현 - formData를 서버액션에 전달하기위해 hiddenInput사용

--- components/review-form.tsx
+++ components/review-form.tsx
@@ -6,26 +6,46 @@ interface IReviewProductProps {
   productId: number;
   sellerId: number;
   sellerName: string;
+  action: (fromData: FormData) => Promise<void>; // serverAction함수를 props로 받음
 }
 
 export default function ReviewForm({
   productId,
   sellerId,
   sellerName,
+  action,
 }: IReviewProductProps) {
   const [rating, setRating] = useState(0);
   return (
-    <form>
-      <div>
-        <span>판매자 {sellerName}</span>
-        {[1, 2, 3, 4, 5].map((star) => (
-          <button key={star} type="button" onClick={() => setRating(star)}>
-            {star <= rating ? "⭐" : "☆"}
-          </button>
-        ))}
+    <form action={action} className="p-5 flex flex-col gap-3 w-full ">
+      <div className="w-full flex gap-3 text-lg">
+        <span className="font-semibold">판매자: {sellerName}</span>
+        <div>
+          {[1, 2, 3, 4, 5].map((star) => (
+            <button key={star} type="button" onClick={() => setRating(star)}>
+              {star <= rating ? "⭐" : "☆"}
+            </button>
+          ))}
+        </div>
       </div>
-      <input type="text" placeholder="리뷰작성" />
-      <button type="submit">완료</button>
+      <input type="hidden" name="rating" value={rating} />
+      <input type="hidden" name="productId" value={productId} />
+      <input type="hidden" name="sellerId" value={sellerId} />
+      <div className="flex flex-col gap-3 w-full h-70">
+        <textarea
+          name="payload"
+          placeholder="거래 경험을 공유해 주세요"
+          className="bg-transparent border rounded-md flex-1 p-3 focus:outline-none focus:ring-2 focus:ring-orange-400 transition"
+        />
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="border-none flex justify-center px-5 py-1 rounded-md bg-orange-500 text-white font-semibold"
+          >
+            완료
+          </button>
+        </div>
+      </div>
     </form>
   );
 }
