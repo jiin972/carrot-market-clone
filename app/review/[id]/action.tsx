3410de242f9a30315b444f0592a937b248f4f14d
jiin972 <jiin972@gmail.com>
2026-05-22 14:17:00 +0900

    feat: review작성 및 DB등록 기능 구현 -  /review/[id]/page.tsx: params로 productId 추출 및 getReviewProduct 서버 액션 호출하여 상품 데이터 조회 -  런타임 오류 방지를 위해 ReviewContent를 Suspense로 래핑
    - /review/[id]/action.tsx: createReview 서버 액션 함수 생성
    
        - FormData를 Zod로 검증하는 로직 추가
        - 검증 완료된 데이터를 바탕으로 DB에 리뷰 정보 등록

--- app/review/[id]/action.tsx
+++ app/review/[id]/action.tsx
@@ -1,5 +1,9 @@
+"use server";
+
 import db from "@/lib/db";
+import getSession from "@/lib/session";
 import { notFound } from "next/navigation";
+import z from "zod";
 
 export async function getReviewProduct(id: number) {
   const product = await db.product.findUnique({
@@ -12,4 +16,39 @@ export async function getReviewProduct(id: number) {
   });
   if (!product) notFound();
   return product;
+}
+
+export async function createReview(formData: FormData) {
+  const session = await getSession();
+  if (!session.id) return; //로그인 안됐으면 종료
+  const data = {
+    productId: formData.get("productId"),
+    sellerId: formData.get("sellerId"),
+    payload: formData.get("payload"),
+    rating: formData.get("rating"),
+  };
+  const reviewSchema = z.object({
+    payload: z.string().min(10, "10자 이상 작성해야 합니다."),
+    rating: z.coerce.number().min(1).max(5), // string->number변환
+    productId: z.coerce.number(), // string->number변환
+    sellerId: z.coerce.number(), // string->number변환
+  });
+  const result = reviewSchema.safeParse(data);
+  if (!result.success) return; // 실패시 종료
+  // if (!result.success) {
+  //   const flatten = z.flattenError(result.error);
+  //   return {
+  //     fieldError: flatten.fieldErrors,
+  //     payload: data,
+  //   };
+  // }
+  const review = await db.review.create({
+    data: {
+      payload: result.data.payload,
+      rating: result.data.rating,
+      productId: result.data.productId,
+      createdById: session.id,
+      createdForId: result.data.sellerId,
+    },
+  });
 }
--- app/review/[id]/page.tsx
+++ app/review/[id]/page.tsx
@@ -1,32 +1,42 @@
-import Image from "next/image";
-import { getReviewProduct } from "./action";
 import ReviewForm from "@/components/review-form";
+import Image from "next/image";
+import { createReview, getReviewProduct } from "./action";
+import { Suspense } from "react";
 
-export default async function Review({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
+async function ReviewContent({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   const productId = Number(id);
   const reviewProduct = await getReviewProduct(productId);
 
   return (
-    <div>
-      <div>
+    <div className="flex flex-col w-full p-5 items-center">
+      <div className="flex flex-col  gap-5">
         <Image
           src={reviewProduct?.photo}
           alt={reviewProduct?.title}
           width={100}
           height={100}
+          className="size-32 rounded-md"
         />
-        <div>{reviewProduct?.title}</div>
+        <div className="text-lg font-semibold">{reviewProduct.title}</div>
       </div>
       <ReviewForm
+        action={createReview}
         productId={productId}
         sellerId={reviewProduct.userId}
         sellerName={reviewProduct.user.username}
       />
     </div>
+  );
+}
+export default async function Review({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  return (
+    <Suspense fallback={"Now loading.."}>
+      <ReviewContent params={params} />
+    </Suspense>
   );
 }
