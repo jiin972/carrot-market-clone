f7ab3d2982b41da988ab6d9fa5ba900d066eb0e9
jiin972 <jiin972@gmail.com>
2026-05-30 10:55:15 +0900

    git commit -m "feat: GitHub OAuth 인증을 위한 GET 라우트 핸들러 추가
    - parameter 정의
    - URLSearchParams로 URL Query String 생성 및 toString 변환"

---
+++ app/(auth)/github/start/route.ts
@@ -0,0 +1,14 @@
+export function GET() {
+  const baseURL = "https://github.com/login/oauth/authorize";
+  //파라메터 작성, 값은 string
+  const params = {
+    client_id: process.env.GITHUB_OAUTH_CLIENTID!, // .env에 있음을 확인!
+    scope: "read:user, read:email",
+    allow_signup: "false",
+  };
+
+  const formattedParams = new URLSearchParams(params).toString();
+  const finalURL = `${baseURL}?${formattedParams}`;
+
+  return Response.redirect(finalURL);
+}
