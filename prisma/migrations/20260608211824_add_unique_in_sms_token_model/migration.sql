dfd559e3019d6fec1e13ccb84a55cbabd9b4238e
jiin972 <jiin972@gmail.com>
2026-06-16 11:59:38 +0900

    chore: remove sqlite migiration history

--- prisma/migrations/20260608211824_add_unique_in_sms_token_model/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[token,phone]` on the table `SMSToken` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "SMSToken_token_phone_key" ON "SMSToken"("token", "phone");
