09c094fa595d3aa018a19341672ffafa5af94287
jiin972 <jiin972@gmail.com>
2026-06-16 11:58:45 +0900

    feat: install neon serverless adapter and update database configuration

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,18 @@
 import "server-only";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+// import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 //어뎁터 생성(prisma7,설정 객체 url필수)
-const adapter = new PrismaBetterSqlite3({
-  url: "file:./database.db",
-});
+// const adapter = new PrismaBetterSqlite3({
+//   url: env("DATABASE_URL"),
+// });
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool as any);
 
 //클라이언트 생성(어탭터 주입)
 const db = new PrismaClient({ adapter });
