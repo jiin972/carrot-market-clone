ccacc445ef11985e6e12427cb85062724881ef1b
jiin972 <jiin972@gmail.com>
2026-06-17 13:40:26 +0900

    fix: 페이지 컴포넌트의 use cache 제거 - use cache는 fetch함수에서만 사용용

--- app/post/create/page.tsx
+++ app/post/create/page.tsx
@@ -1,12 +1,10 @@
 import CreatePostForm from "@/components/create-post-form";
-import { cacheLife, cacheTag } from "next/cache";
 
+//기존의 use cache삭제
+// use cache는 페칭함수에만 사용
 export default async function CreatePost() {
-  "use cache";
-  cacheLife("seconds");
-  cacheTag("posts");
   return (
-    <div className="p-3 ">
+    <div className="p-3">
       <CreatePostForm />
     </div>
   );
